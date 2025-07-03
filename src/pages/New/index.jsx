3543d2785549618a47be7c7e3dffb0eab89124b9
RAFAEL SCHMIDTL <rafael.schmidtl@senacsp.edu.br>
2025-07-02 21:06:31 -0300

    segundo commit

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,78 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import api from '../../services/api';
+import camera from '../../assets/camera.svg';
+
+import './styles.css';
+
 export function New(){
+    const [thumbnail, setThumbnail] = useState(null);
+    // caminho das imagens na pasta upload do backend
+    const urlThumbnail = 'http://localhost:3335/files';
+    // varivél preview = 'http://localhost:3335/files/imagem.png'
+    const preview = `${urlThumbnail}/${thumbnail}`;
+
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+
+    const navigate = useNavigate();
+
+    async function handleSubmit(event){
+        event.preventDefault();
+
+        const data = new FormData();
+        const user_id = localStorage.getItem('user');
+
+        data.append('thumbnail', thumbnail);
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price);
+
+        await api.post('/spots/', data, {
+            headers: { user_id }
+        })
+
+        navigate('/dashboard');
+    }
+
     return(
-        
+        <form onSubmit={handleSubmit}>
+            <label 
+                id='thumbnail'
+                style={{ backgroundImage: `url(${preview})`}}
+                className={thumbnail ? 'has-thumbnail' : ''}
+            >
+            <input type="file" 
+            onChange={event => setThumbnail(event.target.files[0])} />    
+            <img src={camera} alt="Select img" />
+            </label>
+            
+            <label htmlFor="company">*EMPRESA</label>
+            <input type="text" 
+             id='company'
+             placeholder='Sua empresa incrível'
+             value={company}
+             onChange={event => setCompany(event.target.value)}/>
+
+            <label htmlFor="techs">*TECNOLOGIAS (separadas por vírgula)</label>
+            <input type="text" 
+             id='techs'
+             placeholder='Quais tecnologias usam?'
+             value={techs}
+             onChange={event => setTechs(event.target.value)}
+            />
+
+            <label htmlFor="price">*VALOR DA DIÁRIA 
+                (em branco para GRATUITO) </label>
+            <input type="text" 
+             id='price'
+             placeholder='Valor cobrado por dia'
+             value={price}
+             onChange={event => setPrice(event.target.value)}/>    
+
+             <button type='submit' className='btn'>Cadastrar</button>
+        </form>
     )
 }
--- src/root.jsx
+++ src/root.jsx
@@ -5,12 +5,16 @@ import {
     Routes
 } from 'react-router-dom'
 import { Login } from './pages/Login';
+import { Dashboard } from './pages/Dashboard'
+import { New } from './pages/New'
 
 export default function Root(){
     return (
         <Router>
             <Routes>
                 <Route path="/" exact element={<Login />} />
+                <Route path="/dashboard" exact element={<Dashboard />} />
+                <Route path="/new" exact element={<New />} />
             </Routes>
         </Router>
     )
